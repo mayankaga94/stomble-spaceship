9730acfaa378e45a82d00dc1bb98d944107a4b6f
Mayank Agarwal <mayankaga94@gmail.com>
2021-01-19 22:21:07 +1100

    fix nstalled_apps in settings

--- spaceship/settings.py
+++ spaceship/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party
-    'rest_framework'
+    'rest_framework',
     # user-defined
     'core',
     'api'
