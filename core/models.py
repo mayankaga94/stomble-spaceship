67de755e47423d44e6fac67842ab62b9b030998a
Mayank Agarwal <mayankaga94@gmail.com>
2021-01-19 21:45:19 +1100

    add Location and Spaceship model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+# city and planet is unique
+# integers are allowed in city and planet
+class Location(models.Model):
+    city = models.CharField(max_length=255)
+    planet = models.CharField(max_length=255)
+    capacity = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['city','planet'], name='unique_location')
+        ]
+
+    def __str__(self):
+        return self.city + ', ' + self.planet
+    
+    def serialize(self):
+        return {
+            "city" : self.city,
+            "planet" : self.planet,
+            "capacity" : self.capacity,
+        }
+
+    # returns total count of spaceships stationed at location
+    def spaceship_stationed(self):
+        return Spaceship.objects.filter(location=self.id).count()
+
+# should name be unique?
+class Spaceship(models.Model):
+    status_choices = [
+        ('decomissioned',),
+        ('maintenance',),
+        ('operational',)
+    ]
+    
+    name = models.CharField(max_length=255)
+    model_name = models.CharField(max_length=10)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=status_choices, default='op')
+
+    def __str__(self):
+        return self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "model": self.model_name,
+            "location": self.location.serialize(),
+            "status": self.status,
+        }
